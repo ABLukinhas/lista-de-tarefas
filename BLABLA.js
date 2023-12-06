7979e641fca0a13a530faa615ce6b631a93b1f97
fernandapadilha <vitoriafernandaifc@gmail.com>
2023-12-06 08:53:45 -0300

    pronto

--- BLABLA.js
+++ BLABLA.js
@@ -3,46 +3,49 @@ let tarefas = document.getElementById("tarefas");
 let finalizadas = document.getElementById("finalizadas");
 
 function clq() {
-    let nItem = document.createElement("li")  //criei um elemento
+    let nItem = document.createElement("li"); // criei um li
+    nItem.textContent = task.value; // botei o valor do meu input dentro da li
+    tarefas.appendChild(nItem); //botei o li que tem a task dentro do ul com o id tarefas
 
-    nItem.textContent = task.value; //botei o valor de task dentro do elemento novo
+    let nItem2 = document.createElement("button"); // criei um button
 
-    tarefas.appendChild(nItem); //botei o elemento novo dentro da ul com id tarefa
+    //dei nome e class pro button
+    nItem2.innerHTML = "Finalizar";
+    nItem2.className = "BTfinalizar";
+    nItem.appendChild(nItem2);// botei o button dentro da li junto com tudo
 
-    task.value = ""; // deixa o valor de task vazio depois de usar
+    nItem2.addEventListener("click", function() { // dei uma funçao pra o click do button
+        finalizarTarefa(nItem);
+    });
 
-    task.focus();   // sempre foca logo apos escrever
+    task.value = "";//para limpar o input depois de usar
+    task.focus(); // para focar no input depois de usar
+}
 
-    let nItem2 = document.createElement("button")  // criei outro elemento
+function finalizarTarefa(tarefa) {
+    let button = tarefa.querySelector("button"); // ele vai achar o botao de finalizar
+    button.remove(); // e vai remover logo depois de finalizar 
 
-    tarefas.appendChild(nItem2)   // botei dentro da ul com o id tarefas
+    let deleteButton = document.createElement("button"); // criei um botao
 
+    // criei um nome uma class
+    deleteButton.innerHTML = "Excluir";
+    deleteButton.className = "BTexcluir";
+    tarefa.appendChild(deleteButton); // botei o button de delete dentro do ul
 
-    // dei nome e classe  
-    nItem2.innerHTML = "finalizar";
-    nItem2.className = "BTfinalizar"
-    
-    nItem.appendChild(nItem2)
-    
-    }
-    function finalizarTarefa(tarefa) {
-        tarefa.removeChild(tarefa.childNodes[1]); // Vai remover o botão de finalizar a tarefa
-        document.getElementById("finalizadas").appendChild(tarefa); // vai finalizar as tarefas pendentes e leva-las para a parte de excluir a tarefa
-        nItem2.addEventListener("click", function() {
-            finalizarTarefa(task); // Estou adicionando um evento de click no botão
-        });
-    }
-    function excluirTarefa(tarefa) {
-        tarefa.remove(); // Vai remover a tarefa de finalizadas
-    }
+    finalizadas.appendChild(tarefa); // tarefas dentro de finalizadas pra quando aperta finalizadas ficar so o botão de delete e a tarefa na ul
 
- 
+    deleteButton.addEventListener("click", function() {
+        excluirTarefa(tarefa); // dei a funçao de click para o button
+    });
+}
 
-
-
+function excluirTarefa(tarefa) {
+    tarefa.remove();  // vai excluir a tarefa finalizada
+}
 
 task.addEventListener("keypress", (e) => {
-    if (e.key == "Enter") {
+    if (e.key === "Enter") { //   dando a ordem de  quando aperta "Enter" selecionar 
         clq();
     }
-})
+});
