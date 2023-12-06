ffa4060b0e5a71451d6729a21ba7cecb7eb305cc
Lucas Antônio <lucasantoniobrandao008@gmail.com>
2023-12-05 23:17:17 -0300

    arrumando

--- BLABLA.js
+++ BLABLA.js
@@ -3,34 +3,44 @@ let tarefas = document.getElementById("tarefas");
 let finalizadas = document.getElementById("finalizadas");
 
 function clq() {
-    let nItem = document.createElement("li") 
+    let nItem = document.createElement("li")  //criei um elemento
 
-    //criei um elemento
+    nItem.textContent = task.value; //botei o valor de task dentro do elemento novo
 
-    nItem.textContent = task.value;
+    tarefas.appendChild(nItem); //botei o elemento novo dentro da ul com id tarefa
 
-    //botei o valor de task dentro do elemento novo
+    task.value = ""; // deixa o valor de task vazio depois de usar
 
-    tarefas.appendChild(nItem);
+    task.focus();   // sempre foca logo apos escrever
 
-     //botei o elemento novo dentro da ul com id tarefa
+    let nItem2 = document.createElement("button")  // criei outro elemento
 
-    task.value = "";
+    tarefas.appendChild(nItem2)   // botei dentro da ul com o id tarefas
 
-    // deixa o valor de task vazio depois de usar
 
-    task.focus(); 
-
-    // sempre foca logo apos escrever
-
-    let nItem2 = document.createElement("button") 
-
-    // criei outro elemento
-
-    tarefas.appendChild(nItem2)
+    // dei nome e classe  
+    nItem2.innerHTML = "finalizar";
+    nItem2.className = "BTfinalizar"
     
-     // botei dentro da ul com o id tarefas
-} 
+    nItem.appendChild(nItem2)
+    
+    }
+    function finalizarTarefa(tarefa) {
+        tarefa.removeChild(tarefa.childNodes[1]); // Vai remover o botão de finalizar a tarefa
+        document.getElementById("finalizadas").appendChild(tarefa); // vai finalizar as tarefas pendentes e leva-las para a parte de excluir a tarefa
+        nItem2.addEventListener("click", function() {
+            finalizarTarefa(task); // Estou adicionando um evento de click no botão
+        });
+    }
+    function excluirTarefa(tarefa) {
+        tarefa.remove(); // Vai remover a tarefa de finalizadas
+    }
+
+ 
+
+
+
+
 task.addEventListener("keypress", (e) => {
     if (e.key == "Enter") {
         clq();
